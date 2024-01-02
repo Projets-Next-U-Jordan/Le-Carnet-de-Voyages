3ffc4616f42d69c2042bb74638327c5c7838fb06
Jordan <thejordan.dev@gmail.com>
2024-01-02 12:59:02 +0100

    Added: Trips & Users View Controllers

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,4 +1,47 @@
 module Api
     class TripsController < ApplicationController
-    end
+    	before_action :set_trip, only: [:show, :update, :destroy]
+  
+    	# GET /trips
+    	def index
+        	@trips = Trip.all
+        	render json: @trips
+    	end
+  
+    	def show
+        	render json: @trip
+    	end
+  
+		def create
+        	@trip = Trip.new(trip_params)
+  
+        	if @trip.save
+          	render json: @trip, status: :created
+        	else
+          	render json: @trip.errors, status: :unprocessable_entity
+        	end
+      	end
+
+		def update
+			if @trip.update(trip_params)
+				render json: @trip
+			else
+				render json: @trip.errors, status: :unprocessable_entity
+			end
+		end
+  
+		def destroy
+    		@trip.destroy
+		end
+  
+		private
+  
+      	def set_trip
+        	@trip = Trip.find(params[:id])
+    	end
+  
+    	def trip_params
+    		params.require(:trip).permit(:name, :description, :start_date, :end_date, :user_id)
+    	end
+	end
 end
